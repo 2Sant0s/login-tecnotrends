d37e1b1bf19b72606f8c3470b1d3f7eae71c0a32
Josemar dos Santos <Josemardsantoss@outlook.com>
2025-06-25 17:37:49 -0300

    melhorias de usabilidade

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// exibir/oculta senha
+
 const togglePassword = document.querySelector("#togglePassword");
 const password = document.querySelector("#password");
 
@@ -8,3 +10,43 @@ togglePassword.addEventListener("click", function () {
 	this.classList.toggle("fa-eye");
 	this.classList.toggle("fa-eye-slash");
 });
+
+// validação dos campos
+function fieldValidation(event) {
+	var user = document.querySelector('#cpfEmail');
+	var pass = document.querySelector('#password');
+	var infoBox = document.querySelector('#infoBox');
+
+	if (user.value.trim() === '') {
+
+		infoBox.textContent = 'Preencha o campo usuário para continuar';
+		infoBox.style.color = 'red'
+		infoBox.style.opacity = '1';
+		user.style.border = '1px solid red';
+		user.focus();
+		event.preventDefault();
+
+		// oculta a msg e altera o comportamento da borda por de 2.5s
+		setTimeout(() => {
+			user.style.border = '';
+		}, 2000);
+
+	} else if (pass.value.trim() === '') {
+
+		infoBox.textContent = 'Preencha o campo senha para continuar';
+		infoBox.style.color = 'red'
+		infoBox.style.opacity = '1';
+		pass.style.border = '1px solid red';
+		pass.focus();
+		event.preventDefault();
+
+		setTimeout(() => {
+			pass.style.border = '';
+		}, 2000);
+
+	} else {
+		infoBox.textContent = 'Login realizado com sucesso ✅️';
+		infoBox.style.color = 'green'
+		event.preventDefault();
+	}
+}
